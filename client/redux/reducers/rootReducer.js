016ab6e2a2a6d4105e4a9bb7875cd8ed658dc8b8
HeroProtagonist <jstepper91@gmail.com>
2016-06-18 14:41:38 -0700

    Add router to store and added chrome tool

--- client/redux/reducers/rootReducer.js
+++ client/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import places from './placesReducer';
+import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
   places,
+  routing: routerReducer,
 });
 
 export default rootReducer;
--- client/redux/store/configureStore.dev.js
+++ client/redux/store/configureStore.dev.js
@@ -1,12 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+
+const enhancers = compose(
+  applyMiddleware(thunk, reduxImmutableStateInvariant()),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
 
 export default function configureStore(intialState) {
   return createStore(
     rootReducer,
     intialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant()),
+    enhancers,
   );
 }
