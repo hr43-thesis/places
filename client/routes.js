c5d504fcd04322b548978e375d80d9a908268cdb
Adam Lessen <lessena@gmail.com>
2016-06-22 20:44:18 -0700

    (env) set up

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import App from './components/App.jsx';
 import HomeContainer from './components/Home/HomeContainer.jsx';
 import LoginContainer from './components/Login/LoginContainer.jsx';
 import Auth from './components/HOC/Auth.jsx';
+import MyPlaces from './components/Map/Gmap.jsx';
 
 
 export default (
@@ -11,10 +12,8 @@ export default (
     <Route path="/welcome" component={LoginContainer} />
     <Route path="/" component={Auth(App)} >
       <IndexRoute component={HomeContainer} />
+      <Route path="myplaces" component={MyPlaces} />
     </Route>
   </Route>
 );
 
-
-// <IndexRoute component={LoginContainer} />
-// <Route path="home" component={Auth(App)} />
