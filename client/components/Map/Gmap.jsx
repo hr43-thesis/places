865e5aaf016ff265fc3f5ca188e400cd716f8e3a
Adam Lessen <lessena@gmail.com>
2016-06-22 20:23:26 -0700

    (feat) Add map view to show user places
    - Showing dummy data for time being
    - Not using store yet

---
+++ client/components/Map/Gmap.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { GoogleMapLoader, GoogleMap, InfoWindow, Marker } from 'react-google-maps';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+class Maps extends Component {
+  constructor(props) {
+    super(props);
+    }
+
+  render() {
+    return (
+
+    )
+  }
