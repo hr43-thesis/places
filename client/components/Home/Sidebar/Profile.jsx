075d496b02589a990cc901aa26809452eadd7d75
heysep <sdvakili@gmail.com>
2016-07-07 21:00:07 -0700

    Styling of feed component and other components on home page

--- client/components/Home/Sidebar/Profile.jsx
+++ client/components/Home/Sidebar/Profile.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 const Profile = (props) => (
   <div>
     <div className="card-panel blue-grey lighten-5 z-depth-1">
-      Hello {props.name}
+      <h6>Hello {props.name}</h6>
     </div>
     <div className="card-panel grey lighten-5 z-depth-1">
       <img className="center-align responsive-img" alt={props.name} src={props.imageUrl} />
